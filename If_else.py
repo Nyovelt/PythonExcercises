f2a0dd810852cfa04a0659a2f3bda8a2ef78a4c3
Nyovelt <Nyovelt@users.noreply.github.com>
2019-07-05 17:24:19 +0800

    Add

---
+++ If_else.py
@@ -0,0 +1,8 @@
+prettiestName = input("My name is: ")
+while True:
+        name=input("Who is the prettiest girl in the world?: ")
+        if name == prettiestName:
+                print("You're  right!")
+                break
+        else :
+                print("No!!!")
---
+++ firstPY.py
@@ -0,0 +1,8 @@
+
+
+name=input("My name is: ")
+print("'"+name+"'")#第一种写法
+
+print("'",end="")#第二种写法
+print(name,end="")
+print("'")
